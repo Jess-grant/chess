5d76bf9ac0682c886e3cf253c246323a23464172
Jessica Grant <jessicagrant0105@gmail.com>
2023-07-12 14:34:23 +1000

    updated board checking users enter a valid move

--- chess.py
+++ chess.py
@@ -13,24 +13,23 @@ Created on Tue Jul 11 14:05:35 2023
 
 
 board_dim = 8
-
+letters = [' a', 'b', 'c','d','e','f','g','h']
         
 def chess_board():
-           return [['♜', '♞', ' ♝', '♛', '♚', '♝', '♞', '♜'],
-            ['♟︎', '♟︎', ' ♟︎', '♟︎', '♟︎', '♟︎', '♟︎', '♟︎'],
+           return [['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
+            ['♟︎', '♟︎', '♟︎', '♟︎', '♟︎', '♟︎', '♟︎', '♟︎'],
             ['[]', '[]', '[]', '[]', '[]', '[]', '[]', '[]'],
             ['[]', '[]', '[]', '[]', '[]', '[]', '[]', '[]'],
-           ['[]', '[]', '[]', '[]', '[]', '[]', '[]', '[]'],
             ['[]', '[]', '[]', '[]', '[]', '[]', '[]', '[]'],
-            ['♙', '♙', ' ♙', '♙', '♙', '♙', '♙', '♙'],
-            ['♖', '♘', ' ♗', '♕', '♔', '♗', '♘', '♖']]
+            ['[]', '[]', '[]', '[]', '[]', '[]', '[]', '[]'],
+            ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
+            ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']]
       
 def print_board(board):
     
     
     print("  ", end= "")
     for i in range(len(board)):
-        letters = ['  a', 'b', 'c','d','e','f','g','h']
         print(letters[i], end = "  ")
     print()
     
@@ -40,8 +39,43 @@ def print_board(board):
         for j in range(len(board[i])):
             print(board[i][j], end = " ")
         print()
-       
-def main():
+        
+# function that checks if a move is valid
+def validity(board, row, column, new_row, new_column):
+    
+    # check that the row and col are within the range
+    if column in range(len(board)) and row in range(len(board)):
+        return True 
+    else:
+        return False
+    
+
+# a function that moves the piece (assumes it is valid)
+
+def move(board, row, column, new_row, new_column):
+    
+    #print the new chess board
+    print(board[row][column])
+    board[new_row][new_column] = board[row][column]
+    board[row][column] = '[]'
+    print(board)
+    
+# convert user input into board indexes
+def process_move(piece):
+    # takes in the user input string
+    # returns row and column they asked for, as integers
+    # if it's bad, return -1, -1. 
+    
+    row = int(piece[1]) - 1 
+    column = ord(piece[0]) - ord('a')
+    if row in range(board_dim) and column in range(board_dim): 
+        return row, column
+    else:
+        return -1, -1
+    
+    
+
+def game():
     print('Welcome to the Chess game!')
   
     player_turn = "White"
@@ -54,18 +88,37 @@ def main():
         print()
         print_board(board)
         print(f"\n {player_turn} make your move")
+       
+        while True:
+            # Let the player chose which piece they want to move
+            piece = input(" Enter the square of the piece you want to move:")
+            row, column = process_move(piece)
+                              
+                        
+            #Let the player choose the square they want to move too
+            new_position = input(" Where would you like to move to?:")            
+            new_row, new_column = process_move(new_position)
+            print(row, column, new_row, new_column)
         
-        # Let the player chose which piece they want to move
-        piece = input(" Enter the square of the piece you want to move:")
-        old_cell = piece
-        column = ord(old_cell[0]) - ord('a')
-        row = int(old_cell[1]) - 1 
-        
-        #Let the player choose the square they want to move too
-        new_position = input(" Where would you like to move?:")
-        new_column = ord(new_position[0]) - ord('a')
-        new_row = int(new_position[1]) - 1
-        print(" Moving piece from [", piece, "] to [", new_position,"]")
-        break 
-
-main()
+            if row==-1 or column==-1 or new_row==-1 or new_column==-1:
+                print("Hey no bad!")
+            else:
+                break
+            
+            
+        if validity(board, row, column, new_row, new_column):
+            
+                #print the new chess board
+            move(board, row, column, new_row, new_column)
+              
+            print(" Moving piece from [", piece, "] to [", new_position,"]")
+                        
+                        
+                #Switch players
+            if player_turn == "White":
+                    player_turn = "Black"
+            else:
+                    player_turn = "White"
+        else:
+            print("not valid")
+game()
